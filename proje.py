9a982367e2fdbebe91ca5a4667653450387f3a6b
Nisa Nur Tozmaz <121862769+nisatozmaaz@users.noreply.github.com>
2023-04-14 16:28:54 +0300

    proje.py

---
+++ proje.py
@@ -0,0 +1,58 @@
+def üye_ol():
+    input("Kullanıcı adı oluşturunuz: ")
+    input("Şifre oluşturunuz: ")
+    print("Üyeliğiniz başarıyla oluşturulmuştur...\n")
+    
+    
+def giriş_yap():
+    input("Kullanıcı adınızı giriniz: ")
+    input("Şifrenizi giriniz: ")
+    print("Başarılı giriş yapılmıştır...\n")
+    
+    
+def şifre_değişimi():
+    input("Lütfen eski şifrenizi giriniz: ")
+    input("Lütfen yeni şifrenizi giriniz: ")
+    print("Şifreniz başarıyla değiştirilmiştir...\n")
+    
+
+
+
+print("                                Merhabalar Hoşgeldiniz                       ")    
+print("****Öncelikle bölgenizde yaşanan felaket dolayısıyla geçmiş olsun diliyoruz...****")
+
+# Seçeneklerin listesi
+menu = ["1-Sisteme Üye Ol", "2-Sisteme Giriş Yap", "3-Şifremi Değiştirme", "4-Sistemden Çıkış Yap"]
+
+while True:
+    for option in menu:
+        print(option)
+    
+    try:
+        seçim = int(input("Seçiminizi yapın (1-4): "))
+        
+        if seçim == 1:
+            print("Sisteme üye ol seçeneği seçildi.")    
+            üye_ol() 
+            
+        elif seçim == 2:
+            print("Sisteme giriş yap seçeneği seçildi.")
+            giriş_yap()
+                    
+        elif seçim == 3:
+            print("Şifremi değiştirme seçeneği seçildi.")
+            şifre_değişimi()
+                        
+        elif seçim == 4:
+            print("Sistemden çıkış yap seçeneği seçildi. Program sonlandırılıyor...")
+            break
+        
+        else:
+            print("Geçersiz seçenek seçildi! Lütfen tekrar deneyin.")
+            
+            
+    except ValueError:
+        #Girilen değer sayı değilse hata mesajı verir
+        print("Lütfen bir sayı girin!")        
+
+        
