f40d27e0417a156160b978e1d9646f5e77e904b9
Nisa Nur Tozmaz <121862769+nisatozmaaz@users.noreply.github.com>
2023-06-14 00:01:03 +0300

    Update proje.py

--- proje.py
+++ proje.py
@@ -1,41 +1,62 @@
+import pandas as pd2
+
 def üye_ol():
-    input("Kullanıcı adı oluşturunuz: ")
-    input("Şifre oluşturunuz: ")
-    print("Üyeliğiniz başarıyla oluşturulmuştur...\n")
-    
-    
-def giriş_yap():
-    input("Kullanıcı adınızı giriniz: ")
+ ad = input("Adınızı girin: ")
+ soyad = input("Soyadınızı girin: ")
+ sifre = input("Şifrenizi girin: ")
+ print("Üyeliğiniz başarıyla gerçekleşmiştir...")
+ 
+ uye = {"Ad": ad, "Soyad": soyad, "Sifre": sifre}
+ df = pd2.DataFrame([uye])
+ df.to_excel("uyeler.xlsx", index=False) 
+
+  
+def giris():
+    print("Kullanıcı paneline hoş geldiniz...")
+    input("Adınızı giriniz: ")
+    input("Soyadınızı girin: ")
     input("Şifrenizi giriniz: ")
     input("1.Gaziantep\n2.Kahramanmaraş\n3.Hatay\n4.Adana\n5.Diyarbakır\nİlinizi giriniz: ")            
     print("Bulunduğunuz ildeki yardım yoğunluğunun olduğu ilçeler listelenmiştir...\n ")
-    
+   
     #pandas kütüphanesi yardımıyla gerekli ihtiyaçların olduğu excel tablomu okuttum:
     import pandas as pd
     df = pd.read_excel('ihtiyaç.xlsx')
     print(df)
     
     
-def şifre_değişimi():
-    input("Lütfen eski şifrenizi giriniz: ")
-    input("Lütfen yeni şifrenizi giriniz: ")
-    print("Şifreniz başarıyla değiştirilmiştir...\n")
-    
-
+def admin_paneli():
+    print("Admin paneline erişiminiz maalesef yoktur...")
+    print("Kullanıcı paneline yönlendiriliyosunuz.")
 
 
 print("                                Merhabalar Hoşgeldiniz                       ")    
+
 print("****Öncelikle bölgenizde yaşanan felaket dolayısıyla geçmiş olsun diliyoruz...****")
 
+print("Lütfen kullanıcı tipinizi seçin:")
+print("1. Admin")
+print("2. Kullanıcı")
+    
+secim = input("Seçiminizi yapın (1 veya 2): ")
+    
+if secim == "1":
+      admin_paneli()
+elif secim == "2":
+      giris()
+else:
+      print("Geçersiz seçim. Lütfen tekrar deneyin.")
+      
+
 # Seçeneklerin listesi
-menu = ["1-Sisteme Üye Ol", "2-Sisteme Giriş Yap", "3-Şifremi Değiştirme", "4-Sistemden Çıkış Yap"]
+menu1 = ["1-Sisteme Üye Ol", "2-Sisteme Giriş Yap", "3-Sistemden Çıkış Yap"]
 
 while True:
-    for option in menu:
+    for option in menu1:
         print(option)
     
     try:
-        seçim = int(input("Seçiminizi yapın (1-4): "))
+        seçim = int(input("Kullanıcı seçiminizi yapın (1-3): "))
         
         if seçim == 1:
             print("Sisteme üye ol seçeneği seçildi.")    
@@ -43,22 +64,18 @@ while True:
             
         elif seçim == 2:
             print("Sisteme giriş yap seçeneği seçildi.")
-            giriş_yap()
-                    
+            giris()
+          
+    
         elif seçim == 3:
-            print("Şifremi değiştirme seçeneği seçildi.")
-            şifre_değişimi()
-                        
-        elif seçim == 4:
             print("Sistemden çıkış yap seçeneği seçildi. Program sonlandırılıyor...")
             break
         
         else:
             print("Geçersiz seçenek seçildi! Lütfen tekrar deneyin.")
+        
             
             
     except ValueError:
         #Girilen değer sayı değilse hata mesajı verir
-        print("Lütfen bir sayı girin!")        
-
-        
+       print("Lütfen bir sayı girin!")        
